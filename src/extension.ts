8e9a10afb8e14b5a78df0d5e7697788377577137
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-02-20 01:32:17 +0100

    basic command OK

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,112 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const CMD_PREFIX = "alpha.";
+
+const LOOKUP_CMD        = `${CMD_PREFIX}dxtionary.lookup`;
+const LOOKUP_CMD_UI     = `${CMD_PREFIX}dxtionary.lookup.ui`;
+const LOOKUP_CMD_CURSOR = `${CMD_PREFIX}dxtionary.lookup.cursor`;
+const EXTRACT_BUILT_IN_DICT = `${CMD_PREFIX}dxtionary.extract.builtin.dict`;
+
+const normalizedArg = (word:string|undefined) => word && word.trim().length > 0 ? [word] : [""];
+let webviewDictPanelReady = false;
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "alpha-dxtionary" is now active!');
+	console.log(`registry extension`);
+	
+	// every lookup can use command ${LOOKUP_CMD} to perform lookup.
+	const lookupHandler = async (word: string) => {
+		if(word && word.length > 0) {
+			try{
+				let entry = await lookup(word, context);				
+				showEntry(word, entry, context);
+			}catch(ex) {
+				console.log(ex);//log exception
+				vscode.window.showInformationMessage(`something goes wrong as lookup ${word}`);
+			}
+		}else {
+			vscode.window.showInformationMessage(webviewDictPanelReady ? "Wörterbuch ist initialisiert": "Kein Wort zum Nachschlagen" );
+		}
+	};
+	context.subscriptions.push(vscode.commands.registerCommand(LOOKUP_CMD, lookupHandler));
+	
+	// User can trigger dictionary lookup
+	const lookupUIHandler = async () => {
+		let word = await vscode.window.showInputBox({ placeHolder: 'type your looking word up' });
+		const args = normalizedArg(word);
+		vscode.commands.executeCommand(LOOKUP_CMD, ...args)
+			.then(done => {
+				console.log(`success lookup ${done}`);
+			});
+	};
+	context.subscriptions.push(vscode.commands.registerCommand(LOOKUP_CMD_UI, lookupUIHandler));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// User can trigger lookup word under cursor
+	const lookupCursorHandler = async () => {
+		let word = determinateWordUnderCurser();
+		const args = normalizedArg(word);
+		vscode.commands.executeCommand(LOOKUP_CMD, ...args)
+			.then(done => {
+				console.log(`lookup „${word}“ done`);
+			});
+	};
+	context.subscriptions.push(vscode.commands.registerCommand(LOOKUP_CMD_CURSOR, lookupCursorHandler));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Auxiliary commands
+	const extractBuiltinDicts = async() => {
+		let extractMsg = `Please be patient, dxtionary will inform you when extracting is done.
+		Extract dictionary to database file ....`;
+		vscode.window.showInformationMessage(extractMsg);
+		let dictPath = "This variable should save the path to compressed dictionary, which is deliveried with this extension";
+		let dbFile = "This variable should save the path to install directory of this extension on target system and the database sqlite";
+		_extractBuiltinDicts(dictPath, dbFile);
+	};
+	context.subscriptions.push(vscode.commands.registerCommand(EXTRACT_BUILT_IN_DICT, extractBuiltinDicts));
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {}
+
+
+async function lookup(word: string, context: vscode.ExtensionContext):Promise<string>{
+	return Promise.resolve(`TODO: write code to lookup ${word}`);
+}
+
+
+function showEntry(word: string, entry: string, context: vscode.ExtensionContext) {	
+	console.log(`Entry of ${word} in wiktionary.org should be shown.`);
+}
+
+
+function determinateWordUnderCurser(): string|undefined {
+	const { activeTextEditor } = vscode.window;
+
+	// If there's no activeTextEditor, do nothing.
+	if (!activeTextEditor) {
+		return undefined;
+	}
+
+	const { document, selection } = activeTextEditor;
+	const { end, start } = selection;
+	
+	// text too long, so do nothing
+	if (!selection.isSingleLine) {
+		return undefined;
+	}
+	let wordRange:vscode.Range|undefined;
+	if (start.character === end.character){			
+		wordRange = document.getWordRangeAtPosition(start);
+	}else {		
+		wordRange = new vscode.Range(start, end);
+	}
+	if(wordRange) {
+		let highlight = document.getText(wordRange);		
+		return highlight;
+	}else {
+		return undefined;
+	}
+}
+
+function _extractBuiltinDicts(dictPath:string, dbFile:string){
+	console.log(dictPath);
+	console.log(dbFile);
+}
